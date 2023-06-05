9d39f73a5d6f2ca736bc12232a670987c6418f7f
Vadim Yesin <vadim@raceid.com>
2023-06-05 10:10:27 +0300

    init3

--- index.php
+++ index.php
@@ -17,11 +17,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $encodedDescriptions = encodeNewlines($descriptions);
 
-    // Виведення даних за допомогою var_dump()
-    echo "<h2>Дані з форми:</h2>";
-    echo "<pre>";
-    foreach ($encodedDescriptions as $key => $value) {
-        var_dump($titles[$key], $value);
+    echo "<html>";
+    echo "<head>";
+    echo "<title>Сторінка з виведенням даних</title>";
+    echo "</head>";
+    echo "<body>";
+
+    echo "<h2>Заголовки та описи:</h2>";
+
+    for ($i = 0; $i < count($titles); $i++) {
+        echo "<p>";
+        echo "<strong>Заголовок:</strong> " . $titles[$i] . "<br>";
+        echo "<strong>Опис:</strong> " . $descriptions[$i] . "<br>";
+        echo "<button onclick=\"copyContent(this)\">Скопіювати контент</button>";
+        echo "</p>";
     }
-    echo "</pre>";
+
+    echo "<script>";
+    echo "function copyContent(button) {";
+    echo "    var text = button.previousSibling.innerHTML;";
+    echo "    navigator.clipboard.writeText(text);";
+    echo "    alert('Контент скопійовано!');";
+    echo "}";
+    echo "</script>";
+
+    echo "</body>";
+    echo "</html>";
 }
