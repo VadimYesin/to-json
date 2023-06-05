6ce63665c8b8af355e74e2c25e85a014c267b1b2
Vadim Yesin <vadim@raceid.com>
2023-06-05 10:02:50 +0300

    init2

--- index.php
+++ index.php
@@ -4,12 +4,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $titles = $_POST['title'];
     $descriptions = $_POST['description'];
 
+    function encodeNewlines(array $strings) {
+        $result = [];
+        foreach ($strings as $string) {
+            $encodedString = str_replace(array("\r\n", "\r", "\n"), "\\n", $string);
+            $encodedString = str_replace('"', '\"', $encodedString);
+            $result[] = $encodedString;
+        }
+
+        return $result;
+    }
+
+    $encodedDescriptions = encodeNewlines($descriptions);
+
     // Виведення даних за допомогою var_dump()
     echo "<h2>Дані з форми:</h2>";
     echo "<pre>";
-    echo "Заголовки:\n";
-    var_dump($titles);
-    echo "Описи:\n";
-    var_dump($descriptions);
+    foreach ($encodedDescriptions as $key => $value) {
+        var_dump($titles[$key], $value);
+    }
     echo "</pre>";
 }
