2a309dda8f048cfc005e9ff79933f3e313cf0682
Vadim Yesin <vadim@raceid.com>
2023-06-05 22:08:24 +0300

    init21

--- pages/aspects.php
+++ pages/aspects.php
@@ -37,9 +37,9 @@
 <div class="container">
     <h1>Aspects:</h1>
     <form action="/aspects.php" method="POST">
-        <?php foreach ($planets as $planet): ?>
-            <?php foreach ($aspects as $aspect): ?>
-                <?php foreach ($types as $type): ?>
+        <?php foreach ($planets as $planet) : ?>
+            <?php foreach ($types as $type) : ?>
+                <?php foreach ($aspects as $aspect) : ?>
                 <?php if ($planet === $aspect) { continue; } ?>
                     <?php if ($aspect === 'First House') : ?>
                         <h3><?= $planet . ' -> ' . $type . ' -> ' . 'Ascendent' ?></h3>
