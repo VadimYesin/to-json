01e08223017fa400a1158d96b78028729774588a
Vadim Yesin <vadim@raceid.com>
2023-06-05 21:09:45 +0300

    init13

--- parts/header.php
+++ parts/header.php
@@ -15,8 +15,14 @@
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <a href="/index.php" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
                 <a href="/aspects.php" class="nav-link">Aspects</a>
+            </li>
+            <li class="nav-item">
                 <a href="/zodiacs.php" class="nav-link">Zodiacs</a>
+            </li>
+            <li class="nav-item">
                 <a href="/houses.php" class="nav-link">Houses</a>
             </li>
         </ul>
