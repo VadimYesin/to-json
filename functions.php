ea8b66a62ca938cb548adf3a46407a75fefffb56
Vadim Yesin <vadim@raceid.com>
2023-06-06 23:48:16 +0300

    init25

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function encodeDescription($string) {
-    $encodedString = str_replace(array("\r\n", "\r", "\n"), "\\n", $string);
+    $encodedString = str_replace(array("\r\n", "\r", "\n"), "\n", $string);
 
     return str_replace('"', '\"', $encodedString);
 }
